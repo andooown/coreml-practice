f5ba5f9bbc128f9f34c69343c6c190fb33faf7b9
andooown <andooown.dev@gmail.com>
2018-07-13 16:46:07 +0900

    メニュー画面のセルを作成

---
+++ CoreMLPractice/Views/MenuTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MenuTableViewCell.swift
+//  CoreMLPractice
+//
+//  Created by Yoshikazu Ando on 2018/07/13.
+//  Copyright © 2018年 Yoshikazu Ando. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
