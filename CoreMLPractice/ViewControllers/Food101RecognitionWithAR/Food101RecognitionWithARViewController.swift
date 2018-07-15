c9c8d53e69065d2da174ad5662e7a8aad281fb67
andooown <andooown.dev@gmail.com>
2018-07-15 19:01:28 +0900

    一旦コミット

--- CoreMLPractice/ViewControllers/Food101RecognitionWithAR/Food101RecognitionWithARViewController.swift
+++ CoreMLPractice/ViewControllers/Food101RecognitionWithAR/Food101RecognitionWithARViewController.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import ARKit
+import Vision
 
-class Food101RecognitionWithARViewController: UIViewController {
+class Food101RecognitionWithARViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView! {
+        didSet {
+            self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+            self.sceneView.scene = SCNScene()
+        }
+    }
+
+    let queue = DispatchQueue(label: "com.andooown.coreml-practice.recognition-with-ar")
+
+    var mlModel: VNCoreMLModel!
+    var request: VNCoreMLRequest!
+    var isPredicting = false
+    var screenCenter: CGPoint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        do {
+            self.mlModel = try VNCoreMLModel(for: NASNetMobile().model)
+        } catch {
+            print(error.localizedDescription)
+        }
+        self.request = VNCoreMLRequest(model: self.mlModel) { request, _ in
+            guard let bestResult = request.results?.first as? VNClassificationObservation,
+                  bestResult.confidence < 0.3 else {
+                self.isPredicting = false
+                return
+            }
+        }
+        self.request.imageCropAndScaleOption = .centerCrop
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.sceneView.session.run(ARWorldTrackingConfiguration())
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        self.screenCenter = CGPoint(x: self.sceneView.bounds.midX, y: self.sceneView.bounds.midY)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,14 +61,30 @@ class Food101RecognitionWithARViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
+    // MARK: - ARSCNViewDelegate
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        self.hisTest(self.screenCenter!)
+        self.queue.async {
+            guard !self.isPredicting,
+                  let pixelBuffer = self.sceneView.session.currentFrame?.capturedImage else { return }
+            self.isPredicting = true
+
+            let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+            guard (try? handler.perform([self.request])) != nil else {
+                self.isPredicting = false
+                return
+            }
+        }
     }
-    */
+
+    // MARK: - Private Method
+
+    private func hisTest(_ pos: CGPoint) {
+        let nodeResults = self.sceneView.hitTest(pos, options: nil)
+        if nodeResults.count > 0 {
+            print(nodeResults)
+        }
+    }
 
 }
